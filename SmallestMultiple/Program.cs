bad91bd78ab3a48eda9a53e22b976a9ca4c073e7
ROZ <ruoran@gmail.com>
2017-05-30 16:21:33 +0200

    Sovled5

---
+++ SmallestMultiple/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallestMultiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculate(20));
+
+            long product = 1;
+            for (int i = 1; i < 20+1; i++)
+            {
+                product *= i;
+            }
+            Console.WriteLine(product);
+        }
+
+        static int Calculate(int max)
+        {
+            int res = 0;
+            try
+            {
+                bool flag = true;
+                int counter = 1;
+                do
+                {
+                    for (int i = 1; i < max+1; i++)
+                    {
+                        if (counter % i != 0)
+                        { counter++; break; }
+                        
+                        if(i==max)
+                            flag = false;
+                    }
+                } while (flag);
+                res = counter;
+            }
+
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow of number!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+
+            }
+            return res;
+
+        }
+    }
+}
