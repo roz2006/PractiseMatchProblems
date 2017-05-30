c3050b36d9221d75c91a120aacbb1f69f9d317c2
ROZ <ruoran@gmail.com>
2017-05-30 13:07:15 +0200

    ProjectEuler_2solved

---
+++ EvenFibonacci/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculate(4000000));
+        }
+
+        public static int Calculate(int max)
+        {
+            int counter = 1;
+            int sum = 0;
+            int number = 0;
+            do
+            {
+                number = GetNextFibonacci(counter);
+                if (IsEven(number))
+                    sum += number;
+                counter++;
+            } while (number < max);
+
+            return sum;
+        }
+
+
+
+        public static bool IsEven(int n)
+        {
+            bool b = false;
+            if (n % 2 == 0)
+                b = true;
+            return b;
+        }
+
+        public static int GetNextFibonacci(int index)
+        {
+            if ((index == 1) || (index == 2))
+                return index;
+            else
+                return GetNextFibonacci(index - 1) + GetNextFibonacci(index - 2);
+        }
+    }
+}
---
+++ MulitipleOf3And5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MulitipleOf3And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Total3And5Multiple(1000));
+        }
+
+        static int Total3And5Multiple(int max)
+        {
+            int sum = 0;
+            for (int i = 1; i < max; i++)
+            {
+                if ((i % 3 == 0)|| (i % 5 == 0))
+                      sum += i;
+            }
+
+            return sum;
+        }
+    }
+}
