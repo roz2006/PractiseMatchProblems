3a1edbd05e0e38b2bc5dbe23d0878f4c0723ccb1
ROZ <ruoran@gmail.com>
2017-06-08 22:10:38 +0200

    4

--- MaximumPathSumOne/Triangle.cs
+++ MaximumPathSumOne/Triangle.cs
@@ -16,7 +16,7 @@ namespace MaximumPathSumOne
             Numbers = n;
         }
 
-        public Triangle[] MakeSubTriangle(Triangle t)
+        public static Triangle[] MakeSubTriangle(Triangle t)
         {
             int size = t.Numbers.Count() - 1;
             List<int>[] newNumbersLeft = new List<int>[size];
@@ -62,16 +62,39 @@ namespace MaximumPathSumOne
             return numbers;
         }
 
-       public static int CalculateSum(Triangle t)
+        public static int CalculateSum(Triangle t)
         {
-            int sum = 0;
             if (t.Numbers.Count() == 2)
             {
                 int sum1 = t.Numbers[0][0] + t.Numbers[1][0];
                 int sum2 = t.Numbers[0][0] + t.Numbers[1][1];
                 return (sum1 > sum2) ? sum1 : sum2;
             }
-            return sum;
+            else
+            {
+                int subsum = 0;
+                List<int> allSums = new List<int>();
+                while (t.Numbers.Count() > 2)
+                {
+                    Triangle[] subTriangles = MakeSubTriangle(t);
+                    foreach (var st in subTriangles)
+                    {
+                        subsum = CalculateSum(st);
+                        allSums.Add(subsum);
+                    }
+                    int subsumleft = 0;
+                    int subsumright = 0;
+                    for (int i = 0; i < t.Numbers.Count(); i++)
+                    {
+                        subsumleft += t.Numbers[i][0];
+                        subsumright += t.Numbers[i][t.Numbers[i].Count - 1];
+                    }
+                    allSums.Add(subsumleft);
+                    allSums.Add(subsumright);
+                    subsum=allSums.Max();
+                }
+                return subsum;
+            }
         }
     }
 }
